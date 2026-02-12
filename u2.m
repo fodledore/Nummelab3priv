8892363b0e81df7363df6dcdc5619815b85391b4
Fred Amundsen <fredam@kth.se>
2026-02-12 15:04:00 +0100

    Film klar

--- u2.m
+++ u2.m
@@ -1,4 +1,4 @@
-
+%% PARAMETRAR
 N=200; % antal intervall
 T=2; % sluttid
 dx=1/N; % steglängd i rummet
@@ -9,33 +9,51 @@ c=1; % våghastighet
 u=zeros(N-1,M+1); % u(n,m) lösningens värde vid tid (m-1)*dt i position n*dx
 p=zeros(N-1,M+1); % p=u’
 A=zeros(N-1,N-1); % Au är differensapproximation av d^2 u/dx^2
-x = dx*(1:N-1)’; % x(n) är n*dx
+x = dx*(1:N-1)'; % x(n) är n*dx
 E = zeros(1,M+1); % För att beräkna energin i varje tidssteg.
-%Skapa matrisen A
 
-%Sätt begynnelsedata för u och p.
-...
-%Räkna ut energin E vid tiden 0.
+%% Skapa matrisen A
+A = -2*eye(N-1);
+B = zeros(N-1,N-1);
+for i = 1:N-2
+    B(i, i+1) = 1;
+end
+A = 1/(dx)^2*(A +B +B');
+%% Sätt begynnelsedata för u och p.
+g  = @(x) exp(-200*(x-0.5)^2);
+for i = 1:N-1
+    u(i,1) = g(i*dx);
+end
+%Händer inget med p
+%% Räkna ut energin E vid tiden 0.
 ....
 nframe=M+1; % kommando för film
-mov(1:nframe)= struct(’cdata’,[],’colormap’,[]);
+mov(1:nframe)= struct('cdata',[],'colormap',[]);
 figure;
-plot(X,u(:,1), ’b’, ’Linewidth’, 1); %Plot vid tiden t=0.
+plot(X,u(:,1), 'b', 'Linewidth', 1); %Plot vid tiden t=0.
 axis([0 1 -1 1])
-set(gca, ’nextplot’, ’replacechildren’)
+set(gca, 'nextplot', 'replacechildren')
 drawnow
 mov(1)=getframe(gcf); %Första frame i filmen.
-for m=1:M % tidstegning med symplektiska Euler
-...
+
+%% tidstegning med symplektiska Euler
+L=1;
+for m=1:M 
+p(:, m+1) = p(:, m) + dt*A*u(:,m);
+u(:, m+1) = u(:, m) + dt*p(:,m+1) + dt^2*A*u(:,m);
+
 X = [0;x;L]; U = [0;u(:,m);0];
-plot(X, U, ’b’, ’Linewidth’, 1)
+plot(X, U, 'b', 'Linewidth', 1)
 hold on;
-%Plotta även lösningen från d’Alemberts formel
+t = m*dt;
+
+%%Plotta även lösningen från d’Alemberts formel
 .....
-text(0.05,-0.8, sprintf(’t=%.2f’, t))
-set(gca, ’nextplot’, ’replacechildren’)
+text(0.05,-0.8, sprintf('t=%.2f', t))
+set(gca, 'nextplot', 'replacechildren')
 drawnow
 mov(m+1)=getframe(gcf);
+
 %Räkna ut energin av den numeriska lösningen vid detta tidsstag
 .....
 end
